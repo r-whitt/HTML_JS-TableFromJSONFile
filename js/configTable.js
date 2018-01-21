14c30584dd38adcc06ceb8e7738eb3f53b3023fe
r-whitt <rwhitt85@gmail.com>
2018-01-21 12:11:58 -0800

    Updating Table Docs

--- js/configTable.js
+++ js/configTable.js
@@ -1,15 +1,15 @@
-//Pulls tracks from the config file
+//Pulls data from the config file and puts it into a table
 $(function() {
-	var tracks = [];
+	var tableArray = [];
 	$.getJSON('config.json', function(data) {
-		$.each(data.tracks, function(i, f) {
+		$.each(data.tableData, function(i, f) {
             var tr = document.createElement("tr"),
                 html = "";
-            html += "<td class=trackRow>" + f.domain + "</td>";
-            html += "<td class=trackRow>" + f.port + "</td>";
-            html += "<td class=trackRow>" + f.apiKey + "</td>";
+            html += "<td class=tableRow>" + f.URL + "</td>";
+            html += "<td class=tableRow>" + f.protocol + "</td>";
+            html += "<td class=tableRow>" + f.secret + "</td>";
             tr.innerHTML=html;
-			$(tr).appendTo("#trackConfig tbody");
+			$(tr).appendTo("#table tbody");
 		})
 	})
 })
